0eb6d0b92797edc3be6d72b26c01fd6b9b6635ba
Admaxx <56221663+Admaxx@users.noreply.github.com>
2020-12-07 22:20:33 +0100

    Add files via upload

---
+++ ProsteApi/Models/SimpleCalc.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProsteApi.Models
+{
+    public class SimpleCalc
+    {
+        public int Add(int a, int b)
+        {
+
+
+
+            return a + b;
+        }
+        public int Sub(int a, int b)
+        {
+
+
+
+            return a - b;
+        }
+        public int Mul(int a, int b)
+        {
+
+
+
+            return a * b;
+        }
+        public int Div(int a, int b)
+        {
+
+
+
+            return a / b;
+        }
+        public string pass()
+        {
+
+            return "Jajeczko8";
+
+        }
+
+
+
+
+    }
+}
